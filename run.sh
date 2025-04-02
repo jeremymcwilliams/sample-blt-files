a6d2ceeb549b3921ac815de8e4aa99f5331101d2
Jeremy McWilliams <jeremym@lclark.edu>
2025-04-02 12:21:49 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,4 +5,13 @@
 #SBATCH --error=error.txt             # Standard error file
 #SBATCH --time=1:00:00                # Maximum runtime (D-HH:MM:SS)
 
+
+#activate virtual environment
+
+source /home/labs/drake/cs369/venv2025/bin/activate
+
 python3 -u test.py
+
+#exit virtual environment
+deactivate
+
