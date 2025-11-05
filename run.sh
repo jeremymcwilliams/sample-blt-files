36e4e15d6ef8523c436f2c9c93e59d7276be2b59
Jeremy McWilliams <jeremym@lclark.edu>
2025-11-05 11:55:57 -0800

    Add CPU usage comment in run.sh
    Added comment for CPU usage in SLURM script.

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 #SBATCH --output=output.txt           # Standard output file
 #SBATCH --error=error.txt             # Standard error file
 #SBATCH --time=1:00:00                # Maximum runtime (D-HH:MM:SS)
-
+## if using CPUs instead of GPUs, can add something like this: #SBATCH --cpus-per-task=4
 
 #activate virtual environment
 source /home/labs/drake/cs369/venv2025/bin/activate
